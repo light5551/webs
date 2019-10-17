0defbdd3b105c6f11fdc6ac21431ee46c8c5e1e7
DarkFlink <zond.999@yandex.ru>
2019-10-18 02:45:03 +0300

    init

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var lessMiddleware = require('less-middleware');
+//var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 
 var picturesRouter = require('./routes/pictures');
 var pictureRouter = require('./routes/picture');
 var membersRouter = require('./routes/members');
 var optionsRouter = require('./routes/options');
+var auctionRouter = require('./routes/auction');
 
 var app = express();
 
@@ -20,13 +21,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
+//app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/gallery', picturesRouter);
 app.use('/picture', pictureRouter);
 app.use('/members', membersRouter);
 app.use('/options', optionsRouter);
+app.use('/auction', auctionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const less = require('gulp-less')
+const path = require('path');
 
 gulp.task("hello", (callback) => {
     console.log('hello');
@@ -18,5 +20,10 @@ gulp.task('default', (done) => {
             presets: ['@babel/env']
         }))
         .pipe(gulp.dest('public/dist_javascript'));
+    gulp.src('public/stylesheets/style.less')
+        .pipe(less( {
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./public/stylesheets'));
     done()
 });
