ab5a9dd5dc11239cbe3b5db8dea7f2321c9a6bdc
DarkFlink <zond.999@yandex.ru>
2019-10-27 11:18:14 +0300

    'yarn serve' server start

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+const express = require('express');
 
 const port = 4201;
 const host = '127.0.0.1';
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
   res.send('Hello world!');
 });
 
